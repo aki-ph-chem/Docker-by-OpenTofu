93dfb7b06bd30402c9ffa05789077910ac4da3cc
aki <lmxox@proton.me>
2026-08-04 23:09:11 +0900

    flake: Nix Flakes for env

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+        opentofu-with-docker = pkgs.opentofu.withPlugins (p: [
+          p.kreuzwerker_docker
+        ]);
+
+      in
+      pkgs.mkShell {
+        name = "opentofu-docker";
+
+        packages = [
+          opentofu-with-docker
+        ];
+
+        shellHook = ''
+          echo "=========================================="
+          echo "  OpenTofu + Docker devshell"
+          echo "=========================================="
+          echo ""
+          echo ""
+          echo "=========================================="
+        '';
+      };
+  };
+}
